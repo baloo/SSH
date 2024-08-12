8014e43d21d2b3527a40a23251e93f45a752b851
Arthur Gautier <arthur.gautier@arista.com>
2024-08-11 19:25:35 -0700

    ssh-protocol: bring std with feature-flag

--- ssh-protocol/src/lib.rs
+++ ssh-protocol/src/lib.rs
@@ -21,6 +21,9 @@
     unused_qualifications
 )]
 
+#[cfg(feature = "std")]
+extern crate std;
+
 pub use cipher::{self, Cipher};
 pub use encoding::{self, Decode, Encode, Reader, Writer};
 pub use key::{
@@ -41,4 +44,20 @@ impl From<EncodingError> for Error {
     fn from(e: EncodingError) -> Self {
         Self::Encoding(e)
     }
+}
+
+#[cfg(feature = "std")]
+mod error_std {
+    use super::Error;
+    use std::{error, fmt};
+
+    impl fmt::Display for Error {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::Encoding(e) => e.fmt(f),
+            }
+        }
+    }
+
+    impl error::Error for Error {}
 }
